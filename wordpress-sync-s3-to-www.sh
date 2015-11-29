ebc47a2fa51d042238f9a829b9531c7122722b82
Marco Tedone <marco.tedone@gmail.com>
2015-11-29 23:42:19 +0000

    Fixed location of script file

--- wordpress-sync-s3-to-www.sh
+++ wordpress-sync-s3-to-www.sh
@@ -2,6 +2,6 @@
 
 # It invokes the Ansible playbook which synchronises all my Wordpress content from S3 to /var/www/html/
 
-./prepare-aws-server-list.sh default
+$HOME/bin/prepare-aws-server-list.sh default
 
 /usr/local/bin/ansible-playbook ~/bin/ansible/sync-from-s3.yml
--- wordpress-sync-www-to-s3.sh
+++ wordpress-sync-www-to-s3.sh
@@ -2,6 +2,6 @@
 
 # It invokes the Ansible playbook which synchronises the Wordpress website from /var/www/html/ to S3
 
-./prepare-aws-server-list.sh default
+$HOME/bin//prepare-aws-server-list.sh default
 
 /usr/local/bin/ansible-playbook ~/bin/ansible/sync-to-s3.yml
